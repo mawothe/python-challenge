0c79c1aeea4e554ef3051f13f36b48a3b4261414
Marie Wothe <mariewothe@Maries-MacBook-Pro.local>
2019-03-16 21:58:20 -0500

    Runs analysis and prints txt files.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,6 @@
+#Name: Marie Wothe
+#Homework 3: Python Challenge: PyBank
+
 #Dependencies
 import os
 import pandas as pd
@@ -7,21 +10,45 @@ budget_csv = os.path.join("Resources", "budget_data.csv")
 budget_df = pd.read_csv(budget_csv)
 
 #The total number of months included in the dataset
+months_total = len(budget_df)
+print(months_total)
 
 #The net total amount "Profit/Losses" over the entire period
+net_total = sum(budget_df["Profit/Losses"])
 
-#The average of the changes in "Profit/Losses" over the entire period
+#new column with the difference between the current and previous month's p&l
+change = budget_df["Profit/Losses"].diff()
+
+#Add new column with the data
+budget_df["Change in Profit/Losses"] = change
+    
+#The average (mean) of the changes in "Profit/Losses" over the entire period
+average_delta = round(budget_df["Change in Profit/Losses"].mean(),2)
 
 #The greatest increace in profits (date and amount) over the entire period
+greatest_inc = budget_df["Change in Profit/Losses"].max()
 
 #The greatest decrease in losses (date and amount) over the entire period
+greatest_dec = budget_df["Change in Profit/Losses"].min()
 
-#Results
+#Results and Final Formatting
 print("Financial Analysis")
-print(" ")
-print("_______________________________")
-print("Total Months: " + )
-print("Total: " + )
-print("Average Change: " + )
-print("Greatest Increase in Profits: " + )
-print("Greatest Decrease in Profits: " +)
+print("-----------------------------")
+print("Total Months: " + str(months_total))
+print("Total: " + "$" + str(net_total))
+print("Average Change: " + "$" + str(average_delta))
+print("Greatest Increase in Profits: " + budget_df.iloc[25,0] +\
+" ($" + str(greatest_inc) + ")")
+print("Greatest Decrease in Profits: " + budget_df.iloc[44,0]+\
+" ($" + str(greatest_dec) + ")")
+
+#Print to text file
+text_file = open("PyBANK_results.txt", "w")
+text_file.write("""Financial Analysis
+-----------------------------
+Total Months: 86
+Total: $38382578
+Average Change: $-2315.12
+Greatest Increase in Profits: Feb-2012 ($1926159.0)
+Greatest Decrease in Profits: Sep-2013 ($-2196167.0)""")
+text_file.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,9 @@
+#Name: Marie Wothe
+#Homework 3: Python Challenge: PyPoll
+
 #Dependencies
-import os
-import numpy
 import pandas as pd
+import os
 
 #Find and read the data from the resources folder
 voting_file = os.path.join("Resources", "election_data.csv")
@@ -11,31 +13,56 @@ vote = pd.read_csv(voting_file)
 vote_df = pd.DataFrame(vote)
 
 #The total number of votes cast
-total_votes = vote_df.describe
+total = pd.DataFrame(vote_df.count())
+total_votes = total.iloc[1,0]
 
 #A complete number of candidates who received votes
-candidates = vote_df("candidate").unique()
-
-#The percentage of votes each candidate won
-vote_percent = (vote_df("candidate")/total_votes)
+candidates = vote_df["Candidate"].unique()
 
 #The total number of votes each candidate won
-counting_votes = vote_df("candidate").value_counts()
+counting_votes = vote_df["Candidate"].value_counts()
+voter_count = pd.DataFrame(counting_votes)
+
+#The percentage of votes each candidate won
+percent = round(voter_count["Candidate"]/total_votes*100)
+voter_count["Percent of Votes"] = percent
 
 #The winner of the election based on popular vote
+voter_count2 = voter_count.rename(columns = {"Candidate": "Number of Votes"})
 
+#Re-order the columns
+voter_count2 = voter_count2[["Percent of Votes", "Number of Votes"]]
+voter_count3 = pd.DataFrame(voter_count2)
+
+#need to un-index the candidate name 
+final_data = voter_count3.reset_index()
+
+#Final Formatting
+final_data["Number of Votes"] = final_data["Number of Votes"].map("({:,})".format)
+final_data["Percent of Votes"] = final_data["Percent of Votes"].map("{:.2f}%".format)
 
 #Results
+#Print to Terminal
 print("Election Results")
 print("---------------------------")
-print("Total Votes: " + total_votes)
-print(" Person 1 ...")
-print(" Person 2...")
-print(" Person 3...")
+print("Total Votes: " + str(total_votes))
+print("-----------------------------")
+print(final_data.to_string(header= ""))
 print ("--------------------------")
-print(" Winner: " + )
+print(" Winner: " + final_data.iloc[0,0])
 print("----------------------------")
 
-#can you make the results of a calculation done on a data frame 
-#into an new data frame? 
-#can you use the data to make a new table of the data? (a variant of the prev. q.?)
+#Print Results to text file
+text_file = open("PyPoll_results.txt", "w")
+text_file.write("""Election Results
+---------------------------
+Total Votes: 3521001
+-----------------------------
+0      Khan  63.00%  (2,218,231)
+1    Correy  20.00%    (704,200)
+2        Li  14.00%    (492,940)
+3  O'Tooley   3.00%    (105,630)
+--------------------------
+ Winner: Khan
+----------------------------""")
+text_file.close()
